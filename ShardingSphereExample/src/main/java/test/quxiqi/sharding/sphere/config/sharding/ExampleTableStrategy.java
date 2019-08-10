02ed08bdb5200bbe9a56d6d06be9d0df1fb3be87
quxiqi <quxiqi@zskuaixiao.com>
2019-08-10 17:11:08 +0800

    按ID分表

--- ShardingSphereExample/src/main/java/test/quxiqi/sharding/sphere/config/sharding/ExampleTableStrategy.java
+++ ShardingSphereExample/src/main/java/test/quxiqi/sharding/sphere/config/sharding/ExampleTableStrategy.java
@@ -1,41 +1,20 @@
 package test.quxiqi.sharding.sphere.config.sharding;
 
-import com.google.common.collect.Range;
 import org.apache.shardingsphere.api.sharding.standard.PreciseShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
-import org.apache.shardingsphere.api.sharding.standard.RangeShardingAlgorithm;
-import org.apache.shardingsphere.api.sharding.standard.RangeShardingValue;
-import test.quxiqi.sharding.sphere.utils.TableUtils;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
-public class ExampleTableStrategy implements PreciseShardingAlgorithm<Timestamp>, RangeShardingAlgorithm<Timestamp> {
-
-    private static DateTimeFormatter toFormatter   = DateTimeFormatter.ofPattern("yyyyMM");
-
-    @Override
-    public String doSharding(Collection<String> collection, PreciseShardingValue<Timestamp> preciseShardingValue) {
-        LocalDateTime startTime = preciseShardingValue.getValue().toLocalDateTime();
-        String physicalTableName = preciseShardingValue.getLogicTableName() + "_" + startTime.format(toFormatter);
-        TableUtils.createIfNotExist(physicalTableName, preciseShardingValue.getLogicTableName());
-        return physicalTableName;
-    }
+/**
+ * 一个简单的路由策略，主要试验一下 java代码描述路由规则
+ * @author <a href="mailto:quxiqi@zskuaixiao.com"> quxiqi </a>
+ * @version 1.0 2019 八月.2019/8/9
+ */
+public class ExampleTableStrategy implements PreciseShardingAlgorithm<Long> {
 
     @Override
-    public Collection<String> doSharding(Collection<String> collection, RangeShardingValue<Timestamp> rangeShardingValue) {
-        List<String> list = new ArrayList<>();
-        Range<Timestamp> ranges = rangeShardingValue.getValueRange();
-        LocalDateTime startTime = ranges.lowerEndpoint().toLocalDateTime();
-        LocalDateTime endTime = ranges.upperEndpoint().toLocalDateTime();
-        while (startTime.isBefore(endTime)) {
-            list.add(rangeShardingValue.getLogicTableName() + "_" + startTime.format(toFormatter));
-            startTime = startTime.plusMonths(1);
-        }
-        return list;
+    public String doSharding(Collection<String> collection, PreciseShardingValue<Long> preciseShardingValue) {
+        return preciseShardingValue.getLogicTableName() + "_" + preciseShardingValue.getValue()%2;
     }
+
 }
